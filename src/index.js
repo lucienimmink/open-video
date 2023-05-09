543023a497f5e47c342eccbcf0f00034c8395cb7
Lucien Immink <lucien@arielext.org>
2023-05-09 10:08:49 +0200

    Add file selection support to start-banner

--- src/index.js
+++ src/index.js
@@ -10,24 +10,24 @@ const getPreferredWidth = (video) => {
   return window.width < video.videoWidth ? window.width : video.videoWidth;
 };
 
-async function playVideo(fileHandle) {
+async function playVideo(fileHandle, file = null) {
   const errorBanner = document.querySelector(".error-banner");
   errorBanner.classList.add("hidden");
   const video = document.querySelector("video");
-  const fileData = await fileHandle.getFile();
+  const fileData = file || await fileHandle.getFile();
   video.src = URL.createObjectURL(fileData);
   video.controls = true;
   video.play();
 
   video.onloadedmetadata = () => {
-    document.title = `${fileHandle.name}`;
+    document.title = `${fileData.name}`;
     window.resizeTo(getPreferredWidth(video), getPreferredHeight(video));
   };
 
   video.onerror = () => {
     const errorBanner = document.querySelector(".error-banner");
     errorBanner.querySelector(".error-message").textContent =
-      "Error occurred while trying to play " + fileHandle.name;
+      "Error occurred while trying to play " + fileData.name;
     errorBanner.classList.remove("hidden");
   };
 }
@@ -49,3 +49,9 @@ dropElement.addEventListener("drop", async (event) => {
   const fileHandle = await event.dataTransfer.items[0].getAsFileSystemHandle();
   playVideo(fileHandle);
 } );
+
+const fileSelectElement = document.querySelector("input[type=file]");
+fileSelectElement.addEventListener("change", async (event) => { 
+  console.log(fileSelectElement.files[0]);
+  playVideo(null, fileSelectElement.files[0]);
+ });
