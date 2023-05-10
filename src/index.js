a9b260d63a83dd498221c6d9462e34a1fe8da7a1
Lucien Immink <lucien@arielext.org>
2023-05-10 10:48:58 +0200

    Remove shortcuts, add formatting

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import './index.css';
 
-const getPreferredHeight = (video) => {
+const getPreferredHeight = video => {
   const chromeHeight = window.outerHeight - window.innerHeight;
   return window.height < video.videoHeight
     ? window.height
     : video.videoHeight + chromeHeight;
 };
-const getPreferredWidth = (video) => {
+const getPreferredWidth = video => {
   return window.width < video.videoWidth ? window.width : video.videoWidth;
 };
 
 async function playVideo(fileHandle, file = null) {
-  const errorBanner = document.querySelector(".error-banner");
-  errorBanner.classList.add("hidden");
-  const video = document.querySelector("video");
-  const fileData = file || await fileHandle.getFile();
+  const errorBanner = document.querySelector('.error-banner');
+  errorBanner.classList.add('hidden');
+  const video = document.querySelector('video');
+  const fileData = file || (await fileHandle.getFile());
   video.src = URL.createObjectURL(fileData);
   video.controls = true;
   video.play();
@@ -25,41 +25,38 @@ async function playVideo(fileHandle, file = null) {
   };
 
   video.onerror = () => {
-    const errorBanner = document.querySelector(".error-banner");
-    errorBanner.querySelector(".error-message").textContent =
-      "Error occurred while trying to play " + fileData.name;
-    errorBanner.classList.remove("hidden");
+    const errorBanner = document.querySelector('.error-banner');
+    errorBanner.querySelector('.error-message').textContent =
+      'Error occurred while trying to play ' + fileData.name;
+    errorBanner.classList.remove('hidden');
   };
 }
 
-if ("launchQueue" in window) {
-  window.launchQueue.setConsumer((launchParams) => {
+if ('launchQueue' in window) {
+  window.launchQueue.setConsumer(launchParams => {
     if (launchParams.files && launchParams.files.length) {
       playVideo(launchParams.files[0]);
-    }
-    if (launchParams.targetURL) {
-      const query = new URL(launchParams.targetURL).searchParams;
-      const open = query.get('open');
-      if (open) {
-        const fileSelectElement = document.querySelector("input[type=file]");
-        fileSelectElement.click();
-      } 
     }
   });
 }
 
-const dropElement = document.querySelector("video");
-dropElement.addEventListener("dragover", (event) => { event.preventDefault(); dropElement.classList.add('dropping') } );
-dropElement.addEventListener("dragleave", (event) => { event.preventDefault(); dropElement.classList.remove('dropping') } );
-dropElement.addEventListener("drop", async (event) => { 
+const dropElement = document.querySelector('video');
+dropElement.addEventListener('dragover', event => {
   event.preventDefault();
-  dropElement.classList.remove('dropping')
+  dropElement.classList.add('dropping');
+});
+dropElement.addEventListener('dragleave', event => {
+  event.preventDefault();
+  dropElement.classList.remove('dropping');
+});
+dropElement.addEventListener('drop', async event => {
+  event.preventDefault();
+  dropElement.classList.remove('dropping');
   const fileHandle = await event.dataTransfer.items[0].getAsFileSystemHandle();
   playVideo(fileHandle);
-} );
+});
 
-const fileSelectElement = document.querySelector("input[type=file]");
-fileSelectElement.addEventListener("change", async (event) => { 
-  console.log(fileSelectElement.files[0]);
+const fileSelectElement = document.querySelector('input[type=file]');
+fileSelectElement.addEventListener('change', () => {
   playVideo(null, fileSelectElement.files[0]);
- });
+});
--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,45 @@
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default {
   plugins: [
     VitePWA({
-    registerType: "autoUpdate",
+      registerType: 'autoUpdate',
       manifest: {
-        name: "Open Video",
-        display: "fullscreen",
-        orientation: "natural",
-        short_name: "Open Video",
-        description: "Open Video using the browser.",
-        theme_color: "#ffffff",
+        name: 'Open Video',
+        display: 'fullscreen',
+        orientation: 'natural',
+        short_name: 'Open Video',
+        description: 'Open Video using the browser.',
+        theme_color: '#ffffff',
         launch_handler: {
-          client_mode: "focus-existing",
+          client_mode: 'focus-existing',
         },
-        categories: ["entertainment", "video"],
+        categories: ['entertainment', 'video'],
         file_handlers: [
           {
-            action: "/",
+            action: '/',
             accept: {
-              "video/mp4": [".mp4"],
-              "video/webm": [".webm"],
-              "video/ogg": [".ogv"],
-              "video/x-msvideo": [".avi"],
-              "video/x-matroska": [".mkv"],
-              "video/x-m4v": [".m4v"],
-              "video/x-mpeg": [".mpeg"],
-              "video/x-mpeg2": [".mpg"],
+              'video/mp4': ['.mp4'],
+              'video/webm': ['.webm'],
+              'video/ogg': ['.ogv'],
+              'video/x-msvideo': ['.avi'],
+              'video/x-matroska': ['.mkv'],
+              'video/x-m4v': ['.m4v'],
+              'video/x-mpeg': ['.mpeg'],
+              'video/x-mpeg2': ['.mpg'],
             },
-          },
-        ],
-        shortcuts: [
-          {
-            name: "Open file",
-            url: "/?open=true",
-            description: "Browse for a new file to play",
-            icons: [
-              {
-                src: "logo-192x192.png",
-                sizes: "192x192",
-              }
-            ]
           },
         ],
         icons: [
           {
-            src: "logo-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: 'logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "logo-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: 'logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
         ],
       },
